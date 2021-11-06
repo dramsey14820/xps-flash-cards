99341597ad3f7a3c593145cc15004b27d50e8f5d
dramsey14820 <dave.ramsey@outlook.com>
2021-11-06 13:37:41 -0500

    Added support for creating sqlite databases and hashing for sensitive data

---
+++ routes/services/adminService.js
@@ -0,0 +1,3 @@
+// todo:  Can create new databases
+// todo:  can create tables in new database
+// todo:  can upload/attach an external database
---
+++ routes/services/deckEditorService.js
@@ -0,0 +1,6 @@
+// TODO:  Create a deck. (table)
+
+// TODO:  Save the deck to the database
+
+// TODO:  Create a card. (table)
+
